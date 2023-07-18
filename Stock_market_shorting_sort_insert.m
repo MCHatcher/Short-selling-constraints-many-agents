6254ec958e09b6446d788e2f7571d05cf2298c7e
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2023-07-18 12:39:52 +0100

    Add files via upload

--- Stock_market_shorting_sort_insert.m
+++ Stock_market_shorting_sort_insert.m
@@ -1,11 +1,14 @@
 %Stock_market_shorting_sort_insert
+%This code allows the sorting of beliefs and pop. shares in case of ties
+%The main code works without this part
+%Last updated: July 2022. Written by Michael Hatcher (m.c.hatcher@soton.ac.uk) 
    
         %Sort beliefs with ties
            [Beliefs_sort,~,rnk] = unique(Beliefs,'sorted');  %Sorting and ranking        
             
             n_adj = NaN(1,max(rnk));
             for h=1:max(1,max(rnk))
-                V = zeros(1,H);
-                V(rnk == h) = 1;
-                n_adj(1,h) = V*transpose(n);
+                V_00 = zeros(1,H);
+                V_00(rnk == h) = 1;
+                n_adj(1,h) = V_00*transpose(n);
             end
