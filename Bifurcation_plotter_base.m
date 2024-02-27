8a4411b0d538154410092439f7c985377628d28d
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2024-02-27 09:11:25 +0000

    Add files via upload

--- Bifurcation_plotter_base.m
+++ Bifurcation_plotter_base.m
@@ -4,15 +4,17 @@
 figure(1)
 %subplot(1,2,1)
 hold on,
-xlabel('Intensity of choice \beta'), ylabel('Price deviation \it{x}_t'), %title('Absence of short-selling constraints'), %title('Alternative uptick rule')
+xlabel('Intensity of choice \beta'), ylabel('Price deviation \it{x}_t'), %title('Absence of short-selling constraints')
 axis([betta_min,betta_max,-2.2,1]), set(gca, 'box','on')
+plot(betta_stack, x_plot,'o', 'MarkerSize', 2.2, 'Color','k') %[0.5,0.5,0.5]
 
-for v=1:num_betta
+%Slow approach
+%for v=1:num_betta
    
-plot(betta_stack(v), x_plot(:,v),'o', 'MarkerSize', 2.2, 'Color','k') %[0.5,0.5,0.5]
-%plot(betta_stack(1:20), max(x_plot(:,1:20)),'--','Linewidth', 4, 'Color',[1,1,1])
+    %plot(betta_stack(v), x_plot(:,v),'o', 'MarkerSize', 2.2, 'Color','k') %[0.5,0.5,0.5]
+    %plot(betta_stack(1:20), max(x_plot(:,1:20)),'--','Linewidth', 4, 'Color',[1,1,1])
 
-end
+%end
 
 
 
